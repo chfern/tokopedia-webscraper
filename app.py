946bf3d89ef0d4bcdc7447db93919748148f0b71
Fernando Christyanto <christyantofernando@gmail.com>
2018-10-07 12:51:17 +0700

    Add save to csv feature to scraper

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from service.scraper.scraper import Scraper
 from service.scraper.tokopedia_scraper import TokopediaScraper
 tokopedia_scraper = Scraper(TokopediaScraper())
-item_list = tokopedia_scraper.crawl("https://www.tokopedia.com/search?ob=5&st=product&q=photoshop")
-print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+item_list = tokopedia_scraper.crawl("https://www.tokopedia.com/search?ob=5&st=product&q=laravel&page=3")
 print("Item List : {}".format(item_list))
+tokopedia_scraper.to_csv(TokopediaScraper.folder_name, "laravel", item_list)
--- service/scraper/scraper.py
+++ service/scraper/scraper.py
@@ -1,18 +1,27 @@
 from service.driver.firefox import FirefoxDriver
 from bs4 import BeautifulSoup
-import pandas
+import pandas as pd
+import os
 
 class Scraper:
     def __init__(self, scraper):
         self.scraper = scraper
 
     def crawl(self, first_page_url):
-        self.scraper.start_crawl(first_page_url)
+        return self.scraper.start_crawl(first_page_url)
 
     def to_csv(self, folder_name, csv_name, item_list):
         """
         @item_list : a list of list with items : ["title", "price", "location", "shop_name", "rating", "product_img_url", "product_detail_url"]
         """
-        headers = ["title", "price", "location", "shop_name", "rating", "product_img_url", "product_detail_url"]
 
-        dfresult = pandas.Dataframe(item_list, headers)
+        folder_path = "webdata/" + folder_name + "/"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        headers = ["title", "price", "location", "shop_name", "rating", "product_img_url", "product_detail_url"]
+        print("generating csv from itemlist: ")
+        print(item_list)
+        dfresult = pd.DataFrame(item_list, columns=headers)
+
+        dfresult.to_csv(folder_path + csv_name + ".csv", sep=',')
