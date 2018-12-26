a31f568dc45f3f06aa93a58f9904bf6fe24941e7
Fernando Christyanto <christyantofernando@gmail.com>
2018-12-27 00:18:55 +0700

    Change otkopedia scraper test data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from service.scraper.scraper import Scraper
 from service.scraper.tokopedia_scraper import TokopediaScraper
 tokopedia_scraper = Scraper(TokopediaScraper())
-item_list = tokopedia_scraper.crawl("https://www.tokopedia.com/search?ob=5&st=product&q=laravel&page=3")
+item_list = tokopedia_scraper.crawl("https://www.tokopedia.com/search?ob=5&st=product&q=yeezy")
 print("Item List : {}".format(item_list))
-tokopedia_scraper.to_csv(TokopediaScraper.folder_name, "laravel", item_list)
+tokopedia_scraper.to_csv(TokopediaScraper.folder_name, "yeezy", item_list)
